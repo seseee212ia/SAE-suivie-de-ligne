584c7bcbe28973072deeb51677c92e984a0fd009
haby9825 <kcyrprud@gmail.com>
2026-03-30 17:12:32 +0200

    Add files via upload

---
+++ src/Controller/RenduController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Rendu;
+use App\Form\RenduType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+
+#[AsController]
+class UploadRenduAction extends AbstractController
+{
+    public function __invoke(Request $request): Rendu
+    {
+        $rendu = new Rendu();
+        $form = $this->createForm(RenduType::class, $rendu);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $uploadedFile = $form->get('file')->getData();
+
+            if ($uploadedFile) {
+                $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $newFilename = $originalFilename . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
+
+                try {
+                    $uploadedFile->move(
+                        $this->getParameter('rendus_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    throw new BadRequestHttpException('Erreur lors de l\'upload du rendu.');
+                }
+                
+                $rendu->setFileName($newFilename);
+            }
+
+            return $rendu;
+        }
+
+        throw new BadRequestHttpException('Données invalides ou fichier manquant. Vérifiez vos champs.');
+    }
+}
