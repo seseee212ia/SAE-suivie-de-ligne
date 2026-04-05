d2e12ee60c96184ad63685406e9a39e93e1e45c3
sesee_chd <selmachadli79@gmail.com>
2026-04-05 20:44:21 +0200

    Suppression de l'ancien dossier frontend

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import routeur from './routeur.js' // On importe bien "routeur.js"
-
-const app = createApp(App)
-
-app.use(routeur) // On dit à l'application d'utiliser le routeur
-app.mount('#app') // On monte l'application UNE SEULE FOIS
